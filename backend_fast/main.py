6f076d8d2d3d5f249f2c52620dacd541aa22a9b6
TahmidRaven <raven.of.the.endless@gmail.com>
2025-04-28 15:59:49 +0600

    updated commands

---
+++ backend_fast/main.py
@@ -0,0 +1,19 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS — Allow your React app to call the backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For now allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Basic API test route
+@app.get("/")
+async def root():
+    return {"message":"Backend is live but not fully functional yet."}
