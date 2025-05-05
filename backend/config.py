68c36e1f2d5be0c3396d024c079d6a2aa7aabee6
TahmidRaven <raven.of.the.endless@gmail.com>
2025-05-05 17:19:29 +0600

    deleted all files to start new as frontend and backedn was clashing

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://user:password@localhost:5432/optichain")
--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from tortoise import fields
-from tortoise.models import Model
-
-class User(Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=100)
-    email = fields.CharField(max_length=255, unique=True)
-    password = fields.CharField(max_length=128)  # Optional if using Google auth only
-    created_at = fields.DatetimeField(auto_now_add=True)
