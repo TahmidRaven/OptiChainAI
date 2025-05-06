87c1e1c4d4dd01543864b1831a3dc4469be08192
Abdul Khalek Alve <ak.alve1068@gmail.com>
2025-05-06 19:16:58 +0600

    Minor changes

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,15 @@ class User(Model):
 
     def __str__(self):
         return f'{self.username} ({self.email})'
+
+
+class ProductData(Model):
+    id = fields.IntField(pk=True)
+    date = fields.DateField()
+    sku = fields.CharField(max_length=100)
+    sales = fields.IntField()
+    stock_level = fields.IntField()
+    supplier_lead_time_days = fields.IntField()
+    supplier_rating = fields.FloatField()
+    competitor_price_usd = fields.FloatField()
+    our_price_usd = fields.FloatField()
